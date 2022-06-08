095e6a9711a88a0edcf3749a064035e1f199f5ea
Rodrigo Dahlke <rodrigo.dahlke@ecomp.ufsm.br>
2022-06-07 23:49:42 -0300

    ex04 - SO multitarefas

--- MULTITAREFAS/MULTITAREFAS/src/main.c
+++ MULTITAREFAS/MULTITAREFAS/src/main.c
@@ -1,10 +1,34 @@
-#include <stdint.h>
-#include <stdio.h>
+/**
+ * \file
+ *
+ * \brief Exemplos diversos de tarefas e funcionalidades de um sistema operacional multitarefas.
+ *
+ */
 
+/**
+ * \mainpage Sistema operacional multitarefas
+ *
+ * \par Exemplso de tarefas
+ *
+ * Este arquivo contem exemplos diversos de tarefas e
+ * funcionalidades de um sistema operacional multitarefas.
+ *
+ *
+ * \par Conteudo
+ *
+ * -# Inclui funcoes do sistema multitarefas (atraves de multitarefas.h)
+ * -# Inicialização do processador e do sistema multitarefas
+ * -# Criação de tarefas de demonstração
+ *
+ */
+
+/*
+ * Inclusao de arquivos de cabecalhos
+ */
+#include <asf.h>
+#include "stdint.h"
 #include "multitarefas.h"
 
-semaforo cheio= 0;
-semaforo vazio = N;
 
 /*
  * Prototipos das tarefas
@@ -12,17 +36,19 @@ semaforo vazio = N;
 void tarefa_1(void);
 void tarefa_2(void);
 void tarefa_3(void);
-void produtor(void);
-void consumidor(void);
+void tarefa_4(void);
+void tarefa_5(void);
+void tarefa_imprimir(void);
 
 /*
  * Configuracao dos tamanhos das pilhas
  */
-#define TAM_PILHA_1		(TAM_MINIMO_PILHA + 24)
-#define TAM_PILHA_2		(TAM_MINIMO_PILHA + 24)
-#define TAM_PILHA_3		(TAM_MINIMO_PILHA + 24)
-#define TAM_PILHA_4		(TAM_MINIMO_PILHA + 24)
-#define TAM_PILHA_5		(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_1			(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_2			(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_3			(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_4			(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_5			(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_6			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_OCIOSA	(TAM_MINIMO_PILHA + 24)
 
 /*
@@ -33,96 +59,108 @@ uint32_t PILHA_TAREFA_2[TAM_PILHA_2];
 uint32_t PILHA_TAREFA_3[TAM_PILHA_3];
 uint32_t PILHA_TAREFA_4[TAM_PILHA_4];
 uint32_t PILHA_TAREFA_5[TAM_PILHA_5];
+uint32_t PILHA_TAREFA_IMPRIMIR[TAM_PILHA_6];
 uint32_t PILHA_TAREFA_OCIOSA[TAM_PILHA_OCIOSA];
+
+uint32_t cont1 = 0, cont2 = 0, cont3 = 0, cont4 = 0, cont5 = 0, soma = 0;
 
 /*
  * Funcao principal de entrada do sistema
  */
 int main(void)
 {
-	
+	system_init();
+
 	/* Criacao das tarefas */
 	/* Parametros: ponteiro, nome, ponteiro da pilha, tamanho da pilha, prioridade da tarefa */
-	
-	CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 1);
-	
-	CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 2);
-        
-        CriaTarefa(tarefa_3, "Tarefa 3", PILHA_TAREFA_3, TAM_PILHA_3, 3);
-        
-        CriaTarefa(produtor, "Produtor", PILHA_TAREFA_4, TAM_PILHA_4, 4);
-        
-        CriaTarefa(consumidor, "consumidor", PILHA_TAREFA_5, TAM_PILHA_5, 5);
-	
+
+	CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 5); // Maior prioridade
+
+    CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 4);
+
+    CriaTarefa(tarefa_3, "Tarefa 3", PILHA_TAREFA_3, TAM_PILHA_3, 3);
+
+    CriaTarefa(tarefa_4, "Tarefa 4", PILHA_TAREFA_4, TAM_PILHA_4, 2);
+
+    CriaTarefa(tarefa_5, "Tarefa 5", PILHA_TAREFA_5, TAM_PILHA_5, 1); // Menor Prioridade
+
+    CriaTarefa(tarefa_imprimir, "Tarefa Imprimir", PILHA_TAREFA_IMPRIMIR, TAM_PILHA_6, 6);
+
+    /* Suspende todas as Threads exceto a de maior prioridade (tarefa_1) */
+
+    TCB[1].estado = ESPERA;
+    TCB[2].estado = ESPERA;
+    TCB[3].estado = ESPERA;
+    TCB[4].estado = ESPERA;
+
 	/* Cria tarefa ociosa do sistema */
 	CriaTarefa(tarefa_ociosa,"Tarefa ociosa", PILHA_TAREFA_OCIOSA, TAM_PILHA_OCIOSA, 0);
-	
+
 	/* Configura marca de tempo */
-	ConfiguraMarcaTempo();   
-	
+	ConfiguraMarcaTempo();
+
 	/* Inicia sistema multitarefas */
 	IniciaMultitarefas();
-	
+
 	/* Nunca chega aqui */
 	while (1)
 	{
 	}
 }
 
-
-/* Tarefas de exemplo que usam funcoes para suspender/continuar as tarefas */
-void tarefa_1(void)
+void tarefa_1()
 {
-	volatile uint16_t a = 0;
-	for(;;)
-	{
-		a++;
-		TarefaContinua(2);
-	
-	}
+    for (;;)
+    {
+        cont1++;
+        TarefaSuspende(1); // A própria tarefa se suspende
+    }
 }
 
-void tarefa_2(void)
+void tarefa_2()
 {
-	volatile uint16_t b = 0;
-	for(;;)
-	{
-		b++;
-		TarefaSuspende(2);
-                TarefaContinua(3);
-                
-	}
+    for (;;)
+    {
+        cont2++;
+        TarefaContinua(1); // Chama a tarefa de maior prioridade (tarefa_1)
+        TarefaSuspende(2); // A própria tarefa se suspende
+    }
 }
 
-void tarefa_3(void)
+void tarefa_3()
 {
-	volatile uint16_t c = 0;
-	for(;;)
-	{
-		c++;
-		TarefaSuspende(3);	
-	}
+    for (;;)
+    {
+        cont3++;
+        TarefaContinua(2); // Chama a tarefa de maior prioridade (tarefa_2)
+        TarefaSuspende(3); // A própria tarefa se suspende
+    }
 }
 
-void produtor(void)
+void tarefa_4()
 {
-	volatile uint16_t d = 0;
-	for(;;)
-	{
-		wait(vazio);
-                f = (f+1)%N;
-                buffer[f] = produz();
-                signal(cheio);
-	}
+    for (;;)
+    {
+        cont4++;
+        TarefaContinua(3); // Chama a tarefa de maior prioridade (tarefa_3)
+        TarefaSuspende(4); // A própria tarefa se suspende
+    }
 }
-void produtor(void)
+
+void tarefa_5()
 {
-	volatile uint16_t e = 0;
-	for(;;)
-	{
-		wait(vazio);
-                f = (i+1)%N;
-                consome(buffer[i]);
-                signal(cheio);
-	}
+    for (;;)
+    {
+        cont5++;
+        TarefaContinua(4); // Chama a tarefa de maior prioridade (tarefa_4)
+    }
+}
+
+void tarefa_imprimir()
+{
+    for (;;)
+    {
+        soma = cont1 + cont2 + cont3 + cont4 + cont5;
+        TarefaEspera(30);
+    }
 }
